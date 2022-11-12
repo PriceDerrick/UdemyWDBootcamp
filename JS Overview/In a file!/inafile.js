47f0ff612a3af76dce1725c393e41d84f74f4317
Derrick Price <pricemderrick@gmail.com>
2022-11-12 16:09:11 -0500

    yeah

---
+++ JS Overview/In a file!/inafile.js
@@ -0,0 +1,49 @@
+// console.log("Hello World");
+
+// console.log(1+3)
+
+// let rating = 3;
+
+// if (rating === 3) {
+//     console.log("YOU ARE A SUPERSTAR");
+// }
+
+// console.log("Before")
+
+// if (1+1 === 3) {
+//     console.log("Math still works");
+// }
+
+// console.log("After")
+
+
+// console.log("Before 2")
+// let random = Math.random();
+// if (random < 0.5) {
+//     console.log("Your number is less than 0.5");
+//     console.log(random);
+// }
+// console.log("After 2")
+
+// console.log("Before 3")
+// if (random > 0.5) {
+//     console.log("Your number is greater than 0.5");
+//     console.log(random);
+// }
+// console.log("After 3")
+
+function getColor(phrase){
+    //WRITE YOUR CODE BETWEEN THIS LINE: ↓ ↓ ↓ ↓ 
+   if (phrase === "red") {
+       console.log("red");
+   } else if (phrase === "slow"){
+       console.log("yellow");
+   } else if (phrase === "go"){
+       console.log(green);
+   } else console.log("purple");
+   
+    //AND THIS LINE ↑↑↑↑↑
+}
+
+
+
